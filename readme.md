ca0046125261cd8a23a50d988aa70b658b1ecb4b
ankitbhusal <ankitbhusal95@gmail.com>
2023-03-21 09:02:28 +0545

    first commit

---
+++ readme.md
@@ -0,0 +1,5 @@
+## Network Programming
+
+---
+
+Network Programming in Java.
---
+++ src/Main.java
@@ -0,0 +1,21 @@
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        try {
+            Scanner input = new Scanner(System.in);
+            String host = "";
+            System.out.println("Enter the domain name: ");
+            host = input.nextLine();
+            
+            InetAddress address = InetAddress.getByName(host);
+            System.out.println(address.getHostAddress());
+
+        } catch (UnknownHostException e) {
+            System.out.println("Unable to find: " + e);
+        }
+    }
+}
