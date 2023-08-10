8440bfd5735c233ed39f0fa622e503b2661d0033
ankitbhusal <ankitbhusal95@gmail.com>
2023-08-10 18:40:05 +0545

    client server communication using socket

--- src/MyClient.java
+++ src/MyClient.java
@@ -1,2 +1,25 @@
-public class MyClient {
+import java.io.*;
+import java.net.*;
+public class MyClient  {
+    public static void main(String[] args)throws IOException, Exception {
+        Socket s =new Socket("localhost", 3333);
+        DataInputStream din = new DataInputStream(s.getInputStream());
+        DataOutputStream dout = new DataOutputStream(s.getOutputStream());
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String str1 = "";
+        String str2 = "";
+
+        while(!str2.equals("stop")){
+            str1 = br.readLine();
+            dout.writeUTF(str1);
+            dout.flush();
+
+            str2 = din.readUTF();
+            System.out.println("Server says: "+ str2);
+
+
+        }
+        din.close(); dout.close(); s.close();
+    }
 }
--- src/MyServer.java
+++ src/MyServer.java
@@ -1,2 +1,29 @@
-package PACKAGE_NAME;public class MyServer {
+import java.io.*;
+import java.net.*;
+public class MyServer {
+    public static void main(String[] args) throws IOException {
+        ServerSocket ss = new ServerSocket(3333);
+
+        Socket s = ss.accept(); // It establishes the connection to client and wait until request for connection arrival
+
+        DataInputStream din = new DataInputStream(s.getInputStream());
+
+        DataOutputStream dout = new DataOutputStream(s.getOutputStream());
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str1 = "", str2 = "";
+
+        while (!str1.equals("stop")){
+            str1 = din.readUTF();
+            System.out.println("Client: "+ str1);
+
+            str2 = br.readLine();
+
+            dout.writeUTF(str2);
+
+            dout.flush();
+        }
+        din.close(); dout.close(); s.close(); ss.close();
+
+    }
 }
